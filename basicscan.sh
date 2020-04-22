3786d0f1483bf9d2d861de1375607c699386ae19
k3r0sin3 <50089953+k3r0sin3@users.noreply.github.com>
2020-04-22 15:09:11 -0400

    Delete basicscan.sh

--- basicscan.sh
+++
@@ -1,5 +0,0 @@
-echo "Ctrl+C this thing if you aren't running with root privileges!"
-nmap -PN -T4 -A <IP addr> > /tmp/<IP addr>
-
-#make sure to use the NSE located at /usr/share/nmap/scripts
-#Usage example: nmap --script==rdp-enum-encryption.nse,rdp-ntlm-info.nse,rdp-vuln-ms12-020.nse <IP addr> >> /tmp/<IP addr>
